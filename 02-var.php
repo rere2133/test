00303f4e657c60b5cb8c38611a02a36e5ac13495
rere2133 <rere2133@users.noreply.github.com>
2020-08-21 10:54:54 +0800

    08-21

---
+++ 02-var.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    define("MY_CONST", 123);
+    echo MY_CONST;
+
+    echo '<br>';
+    $a = 10;
+    echo $a;
+    echo '<br>';
+    echo empty($b) ? 'empty' :'not empty'.'<br>';
+    echo !empty($b).'<br>';
+    echo '$a<br>';
+    echo "\$a<br>";
+    echo "$a<br>";
+
+    ?>
+</body>
+
+</html>
