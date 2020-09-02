b43624bd37d6e183bf968581124d12187812b832
rere2133 <rere2133@users.noreply.github.com>
2020-09-02 23:34:20 +0800

    09-02

--- 39-edit-product.php
+++ 39-edit-product.php
@@ -1,22 +1,18 @@
 <?php
 require __DIR__ . '/parts/__connect_db.php';
+
 if (!empty($_POST['sid'])) {
-    $sql = "UPDATE `products` SET 
-                `bookname`=?, 
-                `category_sid`=?, 
-                `price`=?,
-                `introduction`=?
-            WHERE `sid`=?";
+    // $sql = "UPDATE `members` SET `mobile`=?,`hash`=?,`nickname`=? WHERE `id`=?";
+    $sql = "UPDATE `products` SET `bookname`=?,`category_sid`=?,`price`=? WHERE `sid`=?";
     $stmt = $pdo->prepare($sql);
+
 
     $stmt->execute([
         $_POST['bookname'],
         $_POST['category_sid'],
         $_POST['price'],
-        json_encode($_POST['hobbies'] ?? []),
-        $_POST['sid'],
+        $_POST['sid']
     ]);
-
     if ($stmt->rowCount()) {
         $modified = true;
     }
@@ -24,18 +20,14 @@ if (!empty($_POST['sid'])) {
 
 $row = $pdo->query("SELECT * FROM products WHERE sid=2")->fetch();
 
-$h_sids = json_decode($row['introduction'], true);
-if ($h_sids === null)
-    $h_sids = [];
-
 $c_sql = "SELECT * FROM categories WHERE parent_sid=0 ORDER BY sid DESC";
-$cates = $pdo->query($c_sql)->fetchAll();
 
-$h_sql = "SELECT * FROM `hobbies` WHERE `visible`=1 ORDER BY `seq`";
-$hobbies = $pdo->query($h_sql)->fetchAll();
+$cate = $pdo->query($c_sql)->fetchAll();
+
 ?>
-<?php require __DIR__ . '/parts/__html_head.php'; ?>
-<?php include __DIR__ . '/parts/__navbar.php'; ?>
+
+<?php include __DIR__ . '/parts/__html_head.php'; ?>
+<?php include __DIR__ . '/parts/__nav.php'; ?>
 <div class="container">
     <?php if (isset($modified)) : ?>
         <div class="alert alert-success" role="alert">
@@ -49,22 +41,29 @@ $hobbies = $pdo->query($h_sql)->fetchAll();
             <input type="text" class="form-control" id="bookname" name="bookname" value="<?= $row['bookname'] ?>">
         </div>
 
-
         <div class="form-group">
-            <label for="category_sid">分類</label>
+            <label for="category_sid2">分類 （後端）</label>
+            <select class="form-control" id="category_sid2">
+                <?php foreach ($cate as $c) : ?>
+                    <option value="<?= $c['sid'] ?>" <?= $row['category_sid'] == $c['sid'] ? 'selected' : '' ?>><?= $c['name'] ?></option>
+                <?php endforeach; ?>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="category_sid">分類 （前端）</label>
             <select class="form-control" id="category_sid" name="category_sid" data-val="<?= $row['category_sid'] ?>">
-                <?php foreach ($cates as $c) : ?>
+                <?php foreach ($cate as $c) : ?>
                     <option value="<?= $c['sid'] ?>"><?= $c['name'] ?></option>
                 <?php endforeach; ?>
             </select>
         </div>
 
         <div class="form-group">
-            <label for="">Hobbies</label><br>
-            <?php foreach ($hobbies as $h) : ?>
+            <label for="category_sid3">分類 (radio button group)</label><br>
+            <?php foreach ($cate as $c) : ?>
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="checkbox" name="hobbies[]" <?= in_array($h['sid'], $h_sids) ? 'checked' : ''  ?> id="hobbies<?= $h['sid'] ?>" value="<?= $h['sid'] ?>">
-                    <label class="form-check-label" for="hobbies<?= $h['sid'] ?>"><?= $h['name'] ?></label>
+                    <input class="form-check-input" type="radio" name="category_sid3" id="cate<?= $c['sid'] ?>" value="<?= $c['sid'] ?>">
+                    <label class="form-check-label" for="cate<?= $c['sid'] ?>"><?= $c['name'] ?></label>
                 </div>
             <?php endforeach; ?>
         </div>
@@ -75,7 +74,6 @@ $hobbies = $pdo->query($h_sql)->fetchAll();
             <input type="text" class="form-control" id="price" name="price" value="<?= $row['price'] ?>">
         </div>
 
-
         <div class="d-flex justify-content-end">
             <input type="submit" value="修改" class="btn btn-primary">
         </div>
@@ -85,7 +83,8 @@ $hobbies = $pdo->query($h_sql)->fetchAll();
     <input type="file" id="file_input" style="display: none">
 
 </div>
-<?php include __DIR__ . '/parts/__scripts.php'; ?>
+
+<?php include __DIR__ . '/parts/__script.php'; ?>
 <script>
     // const file_input = document.querySelector('#file_input');
     // const avatar = document.querySelector('#avatar');
@@ -96,4 +95,4 @@ $hobbies = $pdo->query($h_sql)->fetchAll();
     $category_sid.value = val;
 </script>
 
-<?php include __DIR__ . '/parts/__html_foot.php'; ?>
+<?php include __DIR__ . '/parts/__html_footer.php'; ?>
--- 40-edit-product.php
+++ 40-edit-product.php
@@ -1,13 +1,16 @@
 <?php
 require __DIR__ . '/parts/__connect_db.php';
+
 if (!empty($_POST['sid'])) {
+    // $sql = "UPDATE `members` SET `mobile`=?,`hash`=?,`nickname`=? WHERE `id`=?";
     $sql = "UPDATE `products` SET 
-                `bookname`=?, 
-                `category_sid`=?, 
-                `price`=?,
-                `introduction`=?
+                    `bookname`=?,
+                    `category_sid`=?,
+                    `price`=?,
+                    `introduction`=?
             WHERE `sid`=?";
     $stmt = $pdo->prepare($sql);
+
 
     $stmt->execute([
         $_POST['bookname'],
@@ -16,7 +19,6 @@ if (!empty($_POST['sid'])) {
         json_encode($_POST['hobbies'] ?? []),
         $_POST['sid'],
     ]);
-
     if ($stmt->rowCount()) {
         $modified = true;
     }
@@ -29,13 +31,15 @@ if ($h_sids === null)
     $h_sids = [];
 
 $c_sql = "SELECT * FROM categories WHERE parent_sid=0 ORDER BY sid DESC";
-$cates = $pdo->query($c_sql)->fetchAll();
+$cate = $pdo->query($c_sql)->fetchAll();
 
 $h_sql = "SELECT * FROM `hobbies` WHERE `visible`=1 ORDER BY `seq`";
 $hobbies = $pdo->query($h_sql)->fetchAll();
+
 ?>
-<?php require __DIR__ . '/parts/__html_head.php'; ?>
-<?php include __DIR__ . '/parts/__navbar.php'; ?>
+
+<?php include __DIR__ . '/parts/__html_head.php'; ?>
+<?php include __DIR__ . '/parts/__nav.php'; ?>
 <div class="container">
     <?php if (isset($modified)) : ?>
         <div class="alert alert-success" role="alert">
@@ -49,11 +53,10 @@ $hobbies = $pdo->query($h_sql)->fetchAll();
             <input type="text" class="form-control" id="bookname" name="bookname" value="<?= $row['bookname'] ?>">
         </div>
 
-
         <div class="form-group">
             <label for="category_sid">分類</label>
             <select class="form-control" id="category_sid" name="category_sid" data-val="<?= $row['category_sid'] ?>">
-                <?php foreach ($cates as $c) : ?>
+                <?php foreach ($cate as $c) : ?>
                     <option value="<?= $c['sid'] ?>"><?= $c['name'] ?></option>
                 <?php endforeach; ?>
             </select>
@@ -63,7 +66,7 @@ $hobbies = $pdo->query($h_sql)->fetchAll();
             <label for="">Hobbies</label><br>
             <?php foreach ($hobbies as $h) : ?>
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="checkbox" name="hobbies[]" <?= in_array($h['sid'], $h_sids) ? 'checked' : ''  ?> id="hobbies<?= $h['sid'] ?>" value="<?= $h['sid'] ?>">
+                    <input class="form-check-input" type="checkbox" name="hobbies[]" <?= in_array($h['sid'], $h_sids) ? 'checked' : '' ?> id="hobbies<?= $h['sid'] ?>" value="<?= $h['sid'] ?>">
                     <label class="form-check-label" for="hobbies<?= $h['sid'] ?>"><?= $h['name'] ?></label>
                 </div>
             <?php endforeach; ?>
@@ -75,7 +78,6 @@ $hobbies = $pdo->query($h_sql)->fetchAll();
             <input type="text" class="form-control" id="price" name="price" value="<?= $row['price'] ?>">
         </div>
 
-
         <div class="d-flex justify-content-end">
             <input type="submit" value="修改" class="btn btn-primary">
         </div>
@@ -85,7 +87,8 @@ $hobbies = $pdo->query($h_sql)->fetchAll();
     <input type="file" id="file_input" style="display: none">
 
 </div>
-<?php include __DIR__ . '/parts/__scripts.php'; ?>
+
+<?php include __DIR__ . '/parts/__script.php'; ?>
 <script>
     // const file_input = document.querySelector('#file_input');
     // const avatar = document.querySelector('#avatar');
@@ -96,4 +99,4 @@ $hobbies = $pdo->query($h_sql)->fetchAll();
     $category_sid.value = val;
 </script>
 
-<?php include __DIR__ . '/parts/__html_foot.php'; ?>
+<?php include __DIR__ . '/parts/__html_footer.php'; ?>
